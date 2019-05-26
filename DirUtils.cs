38f8830e5bc6fc7678035440814367517ab3ea57
Hermes Passer <dsldouglas@hotmail.com>
2019-05-26 12:44:37 -0300

    1a commit

---
+++ DirUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.IO;
+
+namespace HUtils
+{
+    public static class DirUtils
+    {
+        public static readonly int MaxPathLength = 259;
+        public static readonly string[] InvalidPathNames = { "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9" };
+        
+        public static bool IsValidAsDir(string path)
+        {
+            string drive = Directory.GetDirectoryRoot(path);
+            if (!Directory.Exists(drive))
+                return false;
+
+            try
+            {
+                Path.GetDirectoryName(path + @"\"); // backlash because otherwise getDirName will c:\\foo\bazz as c:\\foo
+            }
+            catch (Exception e) when (e is ArgumentException || e is PathTooLongException)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+A collection of utility methods i can reuse through projects
